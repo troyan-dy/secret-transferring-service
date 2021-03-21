00c178eb0371a100096a6cda80768680277c917c
troyan-dy <troyan-dy@sber-zvuk.com>
2021-03-21 11:15:12 +0300

    fix password validation

--- secret_transferring_service/types.py
+++ secret_transferring_service/types.py
@@ -1,12 +1,22 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class CreateSecretRequest(BaseModel):
-    expire: int = Field(..., ge=10, le=1000)
+    expire: int = Field(..., ge=10, le=10000)
     message: str = Field(..., min_length=1, max_length=10000)
-    password: int = Field(..., ge=999, lt=100000)
+    password: str
+
+    @validator("password", always=True)
+    def validate_password(cls, value: str) -> str:
+        if len(value) < 5 or len(value) > 12:
+            raise ValueError("Value must be longer then 4 and shorter then 12")
+        try:
+            [int(v) for v in value]
+            return value
+        except Exception:
+            raise ValueError("Value must be valid sting of ints, like 04123")
 
 
 class CreateSecretResponse(BaseModel):
@@ -15,7 +25,17 @@ class CreateSecretResponse(BaseModel):
 
 
 class CheckSecretRequest(BaseModel):
-    password: int
+    password: str
+
+    @validator("password", always=True)
+    def validate_password(cls, value: str) -> str:
+        if len(value) < 5 or len(value) > 12:
+            raise ValueError("Value must be longer then 4 and shorter then 12")
+        try:
+            [int(v) for v in value]
+            return value
+        except Exception:
+            raise ValueError("Value must be valid sting of ints, like 04123")
 
 
 class CheckSecretResponse(BaseModel):
